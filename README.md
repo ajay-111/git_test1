cc028ccad9c3f42d4227b145b15fa3ee3dfc091f
Ajay Kushwaha <ajaykushwahavit@gmail.com>
2022-11-10 12:08:53 +0530

    Developer 2 commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # This is my first readme file created by Ajay Kushwaha
 # I am creating git tutorial
 # This is new readme for developer branch.
+# this update is happeing in developer2 branch.
--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 for i in range(1,11):
     print(i)
+
+for i in ['git',1,2,'hub']:
+    if type(i) == str:
+        print(i, 'is string value')
+    else:
+        print(i, "is numeric value")
+  
